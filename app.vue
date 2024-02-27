33a40b3ea71c9221d1d448b281c83a2d129487c9
ibrahimspider <ibrahimmosstafaspider@gmail.com>
2024-02-27 15:13:40 +0200

    commit

---
+++ app.vue
@@ -0,0 +1,23 @@
+<template>
+  <p>{{ data }}</p>
+</template>
+<script  setup>
+const query = gql`
+query getCharacters{
+  characters{
+    results {
+      name
+      image
+      status
+      id
+      species
+      location {
+        name
+      }
+    }
+  }
+}
+
+`
+const { data } = await useAsyncQuery(query)
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,20 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  css: ["~/assets/css/main.css"],
+
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  modules: ["@nuxtjs/apollo"],
+  apollo: {
+    clients: {
+      default: {
+        httpEndpoint: "https://rickandmortyapi.com/graphql",
+      },
+    },
+  },
+});
